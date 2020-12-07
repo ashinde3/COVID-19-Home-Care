e2a48a0db9f2c2b0de268b5b864390c583065328
Akshay Shinde <akshay@Akshays-MacBook-Pro.local>
2020-12-06 23:07:28 -0500

    config updated 2

--- config.php
+++ config.php
@@ -5,15 +5,4 @@
    define('DB_DATABASE', 'covid');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
-   if (!$db) {
-     die('Could not connect: ' . mysqli_error($db));
-   }
-
-   $db_selected = mysqli_select_db($db, DB_NAME);
-
-   if (!$db_selected) {
-     die('Cannot access' . DB_NAME . ': ' . mysqli_error($db));
-   }
-
-   mysqli_close($db);
 ?>
